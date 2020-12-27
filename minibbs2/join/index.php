d1268263e305137b28beafa2a3f7bbd1ad03997a
takaki <cheap_trick_magic@yahoo.co.jp>
2020-12-27 12:36:00 +0900

    画像をアップロードする①

--- minibbs2/join/index.php
+++ minibbs2/join/index.php
@@ -14,9 +14,19 @@ if (!empty($_POST)) {
   if ($_POST['password'] === '') {
     $error['password'] = 'blank';
   }
+  $fileName = $_FILES['image']['name'];
+  if (!empty($fileName)) {
+    $ext = substr($fileName, -3); // ファイル名の後ろ3文字を切り取り = 拡張子
+    if ($ext != 'jpg' && $ext != 'gif' && $ext != 'png') {
+      $error['image'] = 'type';
+    }
+  }
 
   if (empty($error)) { // エラーが空であればtrue
+    $image = date('YmdHis') . $_FILES['image']['name']; // アップロードするイメージのファイル名を生成する
+    move_uploaded_file($_FILES['image']['tmp_name'], '../member_picture/' . $image);
     $_SESSION['join'] = $_POST;
+    $_SESSION['join']['image'] = $image;
     header('Location: check.php'); // check.phpへジャンプする命令
     exit();
   }
@@ -76,6 +86,9 @@ if ($_REQUEST['action'] === 'rewrite' && isset($_SESSION['join'])) {
           <dt>写真など</dt>
           <dd>
             <input type="file" name="image" size="35" value="test" />
+            <?php if ($error['image'] === 'type') : ?>
+              <p class="error">* 写真などは「.gif」または「.jpg」「.png」の画像を指定してください</p>
+            <?php endif; ?>
           </dd>
         </dl>
         <div><input type="submit" value="入力内容を確認する" /></div>
