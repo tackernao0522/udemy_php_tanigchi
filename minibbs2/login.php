b2c690f5644a9c8088c9752465249d636ea058d9
takaki <cheap_trick_magic@yahoo.co.jp>
2020-12-28 13:43:20 +0900

    ログインの仕組みを作る②

--- minibbs2/login.php
+++ minibbs2/login.php
@@ -14,9 +14,14 @@ if (!empty($_POST)) {
     if ($member) {
       $_SESSION['id'] = $member['id'];
       $_SESSION['time'] = time();
+
       header('Location: index.php');
       exit();
+    } else {
+      $error['login'] = 'failed';
     }
+  } else {
+    $error['login'] = 'blank';
   }
 }
 ?>
@@ -44,11 +49,17 @@ if (!empty($_POST)) {
         <dl>
           <dt>メールアドレス</dt>
           <dd>
-            <input type="text" name="email" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['email']); ?>" />
+            <input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['email'], ENT_QUOTES)); ?>" />
+            <?php if ($error['login'] === 'blank') : ?>
+              <p class="error">* メールアドレスとパスワードをご記入ください</p>
+            <?php endif; ?>
+            <?php if ($error['login'] === 'failed') : ?>
+              <p class="error">* ログインに失敗しました。正しくご記入ください</p>
+            <?php endif; ?>
           </dd>
           <dt>パスワード</dt>
           <dd>
-            <input type="password" name="password" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['password']); ?>" />
+            <input type="password" name="password" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['password'], ENT_QUOTES)); ?>" />
           </dd>
           <dt>ログイン情報の記録</dt>
           <dd>
