f6d6ccb3d62bc44cb91fafb5e0add5ee2121aba9
takaki <cheap_trick_magic@yahoo.co.jp>
2020-12-28 16:13:46 +0900

    メッセージを投稿する②

--- minibbs2/index.php
+++ minibbs2/index.php
@@ -13,56 +13,74 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
   header('Location: login.php');
   exit();
 }
+
+if (!empty($_POST)) { // 投稿するがクリックされた時
+  if ($_POST['message'] !== '') {
+    $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, created=NOW()');
+    $message->execute(array(
+      $member['id'], // データベースから取得した方がより確実である
+      $_POST['message'],
+    ));
+
+    header('Location: index.php'); // 重複しなくなる
+    exit();
+  }
+}
+
+$posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
 ?>
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>ひとこと掲示板</title>
 
-	<link rel="stylesheet" href="style.css" />
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>ひとこと掲示板</title>
+
+  <link rel="stylesheet" href="style.css" />
 </head>
 
 <body>
-<div id="wrap">
-  <div id="head">
-    <h1>ひとこと掲示板</h1>
-  </div>
-  <div id="content">
-  	<div style="text-align: right"><a href="logout.php">ログアウト</a></div>
-    <form action="" method="post">
-      <dl>
-        <dt><?php print(htmlspecialchars($member['name'], ENT_QUOTES)); ?>さん、メッセージをどうぞ</dt>
-        <dd>
-          <textarea name="message" cols="50" rows="5"></textarea>
-          <input type="hidden" name="reply_post_id" value="" />
-        </dd>
-      </dl>
-      <div>
-        <p>
-          <input type="submit" value="投稿する" />
+  <div id="wrap">
+    <div id="head">
+      <h1>ひとこと掲示板</h1>
+    </div>
+    <div id="content">
+      <div style="text-align: right"><a href="logout.php">ログアウト</a></div>
+      <form action="" method="post">
+        <dl>
+          <dt><?php print(htmlspecialchars($member['name'], ENT_QUOTES)); ?>さん、メッセージをどうぞ</dt>
+          <dd>
+            <textarea name="message" cols="50" rows="5"></textarea>
+            <input type="hidden" name="reply_post_id" value="" />
+          </dd>
+        </dl>
+        <div>
+          <p>
+            <input type="submit" value="投稿する" />
+          </p>
+        </div>
+      </form>
+
+      <?php foreach($posts as $post): ?>
+      <div class="msg">
+        <img src="member_picture/<?php print(htmlspecialchars($post['picture'], ENT_QUOTES)); ?>" width="48" height="48" alt="<?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?>" />
+        <p><?php print(htmlspecialchars($post['message'], ENT_QUOTES)); ?><span class="name">（<?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?>）</span>[<a href="index.php?res=">Re</a>]</p>
+        <p class="day"><a href="view.php?id="><?php print(htmlspecialchars($post['created'], ENT_QUOTES)); ?></a>
+          <a href="view.php?id=">
+            返信元のメッセージ</a>
+          [<a href="delete.php?id=" style="color: #F33;">削除</a>]
         </p>
       </div>
-    </form>
+      <?php endforeach; ?>
 
-    <div class="msg">
-    <img src="member_picture" width="48" height="48" alt="" />
-    <p><span class="name">（）</span>[<a href="index.php?res=">Re</a>]</p>
-    <p class="day"><a href="view.php?id="></a>
-<a href="view.php?id=">
-返信元のメッセージ</a>
-[<a href="delete.php?id="
-style="color: #F33;">削除</a>]
-    </p>
+      <ul class="paging">
+        <li><a href="index.php?page=">前のページへ</a></li>
+        <li><a href="index.php?page=">次のページへ</a></li>
+      </ul>
     </div>
-
-<ul class="paging">
-<li><a href="index.php?page=">前のページへ</a></li>
-<li><a href="index.php?page=">次のページへ</a></li>
-</ul>
   </div>
-</div>
 </body>
+
 </html>
