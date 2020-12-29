a1d60ea1498e0ba50910f67eb7ece28e76148f4f
takaki <cheap_trick_magic@yahoo.co.jp>
2020-12-29 11:58:56 +0900

    削除機能を実装

--- minibbs2/index.php
+++ minibbs2/index.php
@@ -83,7 +83,9 @@ if (isset($_REQUEST['res'])) { // REと言うリンクがクリックされた�
               <a href="view.php?id=<?php print(htmlspecialchars($post['reply_message_id'], ENT_QUOTES)); ?>">
                 返信元のメッセージ</a>
             <?php endif; ?>
-            [<a href="delete.php?id=" style="color: #F33;">削除</a>]
+            <?php if ($_SESSION['id'] == $post['member_id']) : ?>
+              [<a href="delete.php?id=<?php print(htmlspecialchars($post['id'])); ?>" style="color: #F33;">削除</a>]
+            <?php endif; ?>
           </p>
         </div>
       <?php endforeach; ?>
