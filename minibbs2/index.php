2d034449c7c3d86429dc5d0d05768634f047eb6b
takaki <cheap_trick_magic@yahoo.co.jp>
2020-12-29 12:29:45 +0900

    ページネーションの実装

--- minibbs2/index.php
+++ minibbs2/index.php
@@ -28,7 +28,23 @@ if (!empty($_POST)) { // 投稿するがクリックされた時
   }
 }
 
-$posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
+$page = $_REQUEST['page'];
+if ($page == '') { // パラメータが何も指定されなかった時
+  $page = 1; // 1とする
+}
+$page = max($page, 1);
+
+$counts = $db->query('SELECT COUNT(*) AS cnt FROM posts');
+$cnt = $counts->fetch();
+$maxPage = ceil($cnt['cnt'] / 5);
+$page = min($page, $maxPage); // maxページにしかならない
+
+$start = ($page - 1) * 5;
+
+$posts = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC LIMIT ?,5');
+$posts->bindParam(1, $start, PDO::PARAM_INT);
+$posts->execute();
+
 
 if (isset($_REQUEST['res'])) { // REと言うリンクがクリックされた場合
   // 返信の処理
@@ -91,8 +107,17 @@ if (isset($_REQUEST['res'])) { // REと言うリンクがクリックされた�
       <?php endforeach; ?>
 
       <ul class="paging">
-        <li><a href="index.php?page=">前のページへ</a></li>
-        <li><a href="index.php?page=">次のページへ</a></li>
+        <?php if ($page > 1) : ?>
+          <li><a href="index.php?page=<?php print($page - 1); ?>">前のページへ</a></li>
+        <?php else : ?>
+          <li>前のページへ</li>
+        <?php endif; ?>
+
+        <?php if ($page < $maxPage) : ?>
+          <li><a href="index.php?page=<?php print($page + 1); ?>">次のページへ</a></li>
+        <?php else : ?>
+          <li>次のページへ</li>
+        <?php endif; ?>
       </ul>
     </div>
   </div>
